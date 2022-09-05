6695d82136a9920b9d2ff38ab152c4a11e2f357f
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-09-05 12:22:13 -0700

    Moved to own repo from WebDevBootcamp. Setup for Heroku.

--- app.js
+++ app.js
@@ -63,6 +63,11 @@ app.post("/failure", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Listening on port 3000.");
 })
